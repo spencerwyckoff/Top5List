a9f2e3766880730d86b43ee419f6253b92089020
Spencer Wyckoff <spencer.wyckoff@gmail.com>
2015-03-02 09:47:02 -0500

    initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## Top 5 Lists
+
+A way to create top 5 Lists and add items to it.
+
+User Login / Register is required to use the app.
+
---
+++ app/scripts/main.js
@@ -0,0 +1,24 @@
+;(function () {
+
+	'use strict';
+
+	angular.module('Top5', ['ngRoute']);
+
+
+	.constant('PARSE', {
+
+		URL: 'https://api.parse.com/1/',
+		CONFIG: {
+			headers: {
+					'X-Parse-Application-Id': 'Lpud5B3WK1T1oZ5zIzoh92wR7Dp5HpCd0HxImwrY',
+					'X-Parse-REST-API-Key': 'HR3kiVtmdoiZbBl2C4fBPFgaKMrCWSOgaTZ6E993',
+					'Content-Type': 'application/json'
+				}
+
+		}
+	})
+
+
+
+
+}());
